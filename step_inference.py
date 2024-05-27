fff685dcf0ba5fcb7f860e2de097e7ee372f8741
AmanVernekar <amansuraj.av@gmail.com>
2024-05-27 11:21:58 +0100

    generated step hmm heatmaps

--- step_inference.py
+++ step_inference.py
@@ -5,71 +5,41 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
 
-# m = 50
-# r = 1
-# x0 = 0.2
-# Rh = 50
-
-# N_trials = 5
-# T = 100
-# dt = 1/T
-
-# step_better = StepHMM_better(m, r, x0, Rh=Rh)
-
-# chains, spikes, jumps, _ = step_better.simulate(N_trials, T)
-# rates = np.array([x0]*(r) + [1])*Rh*(1/T)
-
-# lls = poisson_logpdf(spikes, rates)
-# # print(lls)
-# Ps = step_better.trans_mtx
-# pi = np.array([1] + [0]*r) @ Ps**r
-
-# expected_states, normalizer = hmm_expected_states(pi, Ps, lls[0])
-# print(expected_states[-1])
-
-
-def plot_step_model(m,r,x0,Rh,T,Ntrials):
+def plot_step_model(m,r,x0,Rh,T,N_trials):
     step = StepHMM_better(m = m,r = r,x0= x0, Rh = Rh)
-    chains, spikes, jumps, _ = step.simulate(Ntrials, T)
-    # pi0 = step.p0
-    # Ps = step.transition_matrix
-    # Ps = np.array([Ps])
-    # xt_list = np.ones(r+1)*x0
-    # xt_list[-1] = 1
-    # rates_list = Rh * xt_list
+    _, spikes, jumps, _ = step.simulate(N_trials, T)
     rates = np.array([x0]*(r) + [1])*Rh*(1/T)
-    ll = poisson_logpdf(spikes,rates)#what matters is the mean count not the rate
-    # print(ll)
+    ll = poisson_logpdf(spikes,rates)
     
     Ps = step.trans_mtx
     pi = np.array([1] + [0]*r) @ Ps**r
     
-    marker_styles = [None, 'o', 'x']
+    marker_styles = [None, 's', 'x']
     line_styles = ['solid', None, None]
-    labels = [r'$P(s_t = 1 | n_{1:T})$', 'Inferred jump time', 'True jump time']
+    labels = ['Posterior probability', 'Inferred jump time', 'True jump time']
 
     fig, ax = plt.subplots(1,2)
-    plt.suptitle('m = {}, r = {}, x0 = {}, Rh = {}'.format(m,r,x0,Rh))
-    # fig.tight_layout()
+    plt.suptitle(f'Probability of being in the upper rate level\nm = {m}, r = {r}, x0 = {x0}, Rh = {Rh}')
+    fig.tight_layout()
     fig.set_figheight(4)
     fig.set_figwidth(10) 
-    for i_trial in range(Ntrials):
+    for j in range(N_trials):
         for i in range(2):
-            expected_states ,_= hmm_expected_states(pi,Ps,ll = ll[i_trial,:,:], filter=i)# each row is posterior distribution at a given time
+            expected_states ,_= hmm_expected_states(pi,Ps,ll = ll[j,:,:], filter=i)# each row is posterior distribution at a given time
         #shape(expected_states)= T,K
             high_state_probability = expected_states[:,r]
-            ax[i].plot(np.arange(T),high_state_probability, color= "C{}".format(i_trial))
+            ax[i].plot(np.arange(T),high_state_probability, color= "C{}".format(j))
 
             jump_time = np.where(high_state_probability>= 0.5)
             if len(jump_time[0]) > 0:
                 jump_time = jump_time[0][0]
-                # plt.axvline(jump_time, color= "C{}".format(i_trial), linestyle='dotted', label='Inferred jump time')
-                ax[i].scatter(jump_time, 0.5, color= "C{}".format(i_trial), marker =marker_styles[1])
+                # plt.axvline(jump_time, color= "C{}".format(j), linestyle='dotted', label='Inferred jump time')
+                ax[i].scatter(jump_time, 0.5, color= "C{}".format(j), marker =marker_styles[1])
 
             # Only plot the simulated jump if we have actually jumped
-            if jumps[i_trial] != -1:
-                # plt.axvline(jump[i_trial], color = "C{}".format(i_trial), linestyle = 'dashed', label='True jump time')
-                ax[i].scatter(jumps[i_trial], 0.5, color = "C{}".format(i_trial), marker =marker_styles[2])
+            if jumps[j] != -1:
+                # plt.axvline(jump[j], color = "C{}".format(j), linestyle = 'dashed', label='True jump time')
+                ax[i].scatter(jumps[j], 0.5, color = "C{}".format(j), marker =marker_styles[2])
 
     dummy_lines = []
     for i in range(len(marker_styles)):
@@ -81,11 +51,75 @@ def plot_step_model(m,r,x0,Rh,T,Ntrials):
     plt.show()
 
 
-plot_step_model(m=20,r=10,x0=0.2,Rh=50,T=10,Ntrials=1)
-plot_step_model(m=50,r=1,x0=0.2,Rh=50,T=100,Ntrials=5)
-plot_step_model(m=50,r=10,x0=0.8,Rh=50,T=100,Ntrials=5)
-plot_step_model(m=50,r=1,x0=0.8,Rh=50,T=100,Ntrials=5)
+# plot_step_model(m=30,r=10,x0=0.2,Rh=50,T=100,N_trials=5)
+# # plot_step_model(m=30,r=100,x0=0.2,Rh=50,T=100,N_trials=5)
+# plot_step_model(m=50,r=1,x0=0.2,Rh=50,T=100,N_trials=5)
+# plot_step_model(m=50,r=10,x0=0.2,Rh=50,T=100,N_trials=5)
+# # plot_step_model(m=50,r=100,x0=0.2,Rh=50,T=100,N_trials=5)
 
-plot_step_model(m=50,r=10,x0=0.8,Rh=50,T=100,Ntrials=5)
+# plot_step_model(m=30,r=10,x0=0.7,Rh=50,T=100,N_trials=5)
+# # plot_step_model(m=30,r=100,x0=0.7,Rh=50,T=100,N_trials=5)
+# plot_step_model(m=50,r=1,x0=0.7,Rh=50,T=100,N_trials=5)
+# plot_step_model(m=50,r=10,x0=0.7,Rh=50,T=100,N_trials=5)
+# # plot_step_model(m=50,r=100,x0=0.7,Rh=50,T=100,N_trials=5)
 
-plot_step_model(m=100,r=10,x0=0.8,Rh=50,T=100,Ntrials=5)
+
+def calc_avg_error(m, r, x0, Rh, T, N_trials, _filter=False):
+    step = StepHMM_better(m = m,r = r,x0= x0, Rh = Rh)
+    _, spikes, jumps, _ = step.simulate(N_trials, T)
+    jumps[jumps==np.inf] = T
+    rates = np.array([x0]*(r) + [1])*Rh*(1/T)
+    ll = poisson_logpdf(spikes,rates)
+    
+    Ps = step.trans_mtx
+    pi = np.array([1] + [0]*r) @ Ps**r
+    
+    mse = np.zeros((N_trials))
+
+    for j in range(N_trials):
+        expected_states, _ = hmm_expected_states(pi,Ps,ll = ll[j], filter=_filter)
+        high_state_probability = expected_states[:,r]
+        jump_time = T
+        for k, prob in enumerate(high_state_probability):
+            if prob >= 0.5:
+                jump_time = k
+                break
+        mse[j] = ((jump_time - jumps[j])/T)**2
+    
+    return np.mean(mse)
+
+def step_heatmap(m, Rh=50, T=100, N_trials=10, _filter=False):
+    num_vals = 10
+    
+    r = range(1,11,1)
+    x0 = np.linspace(0.1, 1, num_vals)
+    
+    avg_errors = np.zeros([num_vals, num_vals])
+
+    # Compute the return values of the inner function for each combination of varying parameters
+    for i, val1 in enumerate(r):
+        for j, val2 in enumerate(x0):
+            avg_errors[i][j] = calc_avg_error(m, val1, val2, Rh, T, N_trials, _filter)
+    
+    
+
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(avg_errors)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(r)), labels=r)
+    ax.set_yticks(np.arange(len(x0)), labels=x0)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
+    
+    cbar = ax.figure.colorbar(im, ax=ax)
+    cbar.ax.set_ylabel('yo', rotation=-90, va="bottom")
+
+    ax.set_title("Harvest of local farmers (in tons/year)")
+    fig.tight_layout()
+    plt.show()
+
+step_heatmap(m=30)
--- test_hmm_step.py
+++ test_hmm_step.py
@@ -5,21 +5,33 @@ m=50
 r=100
 x0=0.2
 
-N_trials = 5000
+N_trials = 10
 T = 100
 
-# step_naive = StepHMM_naive(m,r,x0)
-# step_better = StepHMM_better(m,r,x0)
+step_naive = StepHMM_naive(m,r,x0)
+step_better = StepHMM_better(m,r,x0)
 
-# chains, spikes, jumps, rates = step_better.simulate(N_trials, T)
-# plt.plot(chains[0])
-# plt.show()
-
-plt.xlim((0,T))
-for r in [1,5,10,100,1000]:
-    step_naive = StepHMM_naive(m,r,x0)
-    chains, spikes, jumps, rates = step_naive.simulate(N_trials, T)
-    plt.hist(jumps[jumps <= T], bins=int(T/10), label=f"{r=}")
-plt.title(f"Jump times for naive Step HMM, {m=}")
+chains, spikes, jumps, rates = step_naive.simulate(N_trials, T)
+for i in range(N_trials):
+    chain = chains[i]
+    chain[chain==0.2] = 0
+    plt.plot(chain, label = f'Markov Chain {i}')
+plt.title(f'Markov Chains for naive Step HMM {m = } {r = }')
+# plt.ylim((0,r+10))
 plt.legend()
 plt.show()
+
+for i in range(N_trials):
+    plt.plot(rates[i], label = f'Firing rate in trial {i}')
+plt.title(f'Firing rates for naive Step HMM {m = } {r = } {x0 = }')
+plt.legend()
+plt.show()
+
+# plt.xlim((0,T))
+# for r in [1,5,10,100,1000]:
+#     step_naive = StepHMM_naive(m,r,x0)
+#     chains, spikes, jumps, rates = step_naive.simulate(N_trials, T)
+#     plt.hist(jumps[jumps <= T], bins=int(T/10), label=f"{r=}")
+# plt.title(f"Jump times for naive Step HMM, {m=}")
+# plt.legend()
+# plt.show()
